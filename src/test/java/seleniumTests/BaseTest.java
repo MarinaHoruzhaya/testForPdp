b6bf1f24379a866aa8bc717b1262e14c92710fad
ostkhm4 <marina.khoruzhaya@partners.raiffeisen.ru>
2025-08-12 13:06:57 +0300

    test

--- src/test/java/seleniumTests/BaseTest.java
+++ src/test/java/seleniumTests/BaseTest.java
@@ -5,14 +5,24 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.UUID;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeEach
     public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/chrome-profile-" + UUID.randomUUID());
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless=new"); // для запуска в headless-режиме
+        options.addArguments("--remote-allow-origins=*"); // для совместимости
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.get("https://www.selenium.dev/");
     }
 
